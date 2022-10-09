653096e0e5ad509f0c7da1afda8a2925df226a0d
Murat1ck <muratoff101088@gmail.com>
2022-10-09 12:17:30 +0300

    Задание №2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        int contribution = 15000;
-        int total = 0;
         int i = 0;
-            while (total < 2_459_000) {
-                total = total + total / 100;
-                total = total + contribution;
-                i = i +1;
+        while (i < 10) {
+            i = i + 1;
+            System.out.print(i + " ");
+        }
+        System.out.println();
+            for (i = 10; i > 0; i--) {
 
-
-                System.out.println("Месяц " + i + " ,cумма накоплений равна " + total + "рублей.");
+                System.out.print(i + " ");
             }
         }
-    }
+   }
+
